6855547a50afe665694a71751a486c60e00d56ee
skahmed <skahmed@myamu.ac.in>
2021-02-05 00:53:44 +0530

    Worked on IncomeExpenses.js to display income and expense from state

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) =>
+    parseFloat(transaction.amount)
+  );
+
+  const income = amounts
+    .filter((amount) => amount > 0)
+    .reduce((acc, item) => acc + item, 0)
+    .toFixed(2);
+
+  const expense = amounts
+    .filter((amount) => amount < 0)
+    .reduce((acc, item) => acc + item, 0)
+    .toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
         <p id="money-plus" className="money plus">
-          +Rs.0.00
+          Rs.{income}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p id="money-minus" className="money minus">
-          -Rs.0.00
+          Rs.{Math.abs(expense)}
         </p>
       </div>
     </div>
