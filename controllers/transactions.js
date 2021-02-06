fb676c2145e69f3681e24140ff6b66df05f2a36a
skahmed <skahmed@myamu.ac.in>
2021-02-06 20:07:52 +0530

    Added controllers for routes

---
+++ controllers/transactions.js
@@ -0,0 +1,6 @@
+// @desc        Get all transactions
+// @route       GET /api/v1/transactons
+// @access      Public
+exports.getTransactions = (req, res, next) => {
+  res.send("GET Transactions");
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { getTransactions } = require("../controllers/transactions");
 
-router.get("/", (req, res) => res.send("Hello"));
+// router.get("/", (req, res) => res.send("Hello"));
+
+router.route("/").get(getTransactions);
 
 module.exports = router;
