a044caa8f6b9312867ccc60d73ca54217472a623
skahmed <skahmed@myamu.ac.in>
2021-02-09 21:02:24 +0530

    Production build

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -14,12 +15,20 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/api/v1/transactions", transactions);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 // app.get("/", (req, res) => res.send("Hello"));
-app.use("/api/v1/transactions", transactions);
 
 const PORT = process.env.PORT || 5000;
 
